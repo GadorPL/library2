83d9d2b8a721f8d2908616285079ddd2041bab00
Michał Hewelt <mhewelt9@gmail.com>
2022-12-20 13:02:32 +0100

    add user creation

--- catalog/views.py
+++ catalog/views.py
@@ -3,6 +3,8 @@ from .models import Book, Author, BookInstance, Genre, Language
 from django.views.generic import CreateView, DetailView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -32,3 +34,9 @@ class BookDetail(DetailView):
 @login_required()
 def my_view(request):
     return render(request, 'catalog/my_view.html')
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'catalog/signup.html'
